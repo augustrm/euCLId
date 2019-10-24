fee14170498eaa13801420a290e8bd0c28af7571
augustrm <35844627+augustrm@users.noreply.github.com>
2019-10-24 12:24:34 -0600

    Update euCLId.py

--- euCLId.py
+++ euCLId.py
@@ -1,19 +1,28 @@
 import turtle
 window = turtle.Screen
-seac = turtle.Turtle()
+
 #stylistic note: seac stands for Straight Edge And Compass
+seac = turtle.Turtle()
 seac.ht()
-turtle.tracer()
+seac.speed(0)
 seac.home()
 seac.pu()
 
+#define a point, its coordinates are arbitary:
 A = (-200,100)
 seac.setpos(A)
 seac.dot()
 
+#define a point, its coordinates are arbitary:
 B = (100, 200)
 seac.setpos(B)
 seac.dot()
+
+#circle class object under construction as of October 24, 2019
+#class euCircle:
+#    def __init__():
+        
+
 
 def circthru(ptA, ptB):
 	seac.setpos(ptB[0], ptB[1])
@@ -25,3 +34,4 @@ def circthru(ptA, ptB):
 	
 circthru(A,B)
 circthru(B,A)
+turtle.exitonclick()
