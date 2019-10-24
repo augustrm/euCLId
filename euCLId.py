dd6d87ca60ceaa2e80dba1d63d47a8e076639b05
augustrm <35844627+augustrm@users.noreply.github.com>
2019-10-24 15:38:40 -0600

    Update euCLId.py

--- euCLId.py
+++ euCLId.py
@@ -1,4 +1,5 @@
 import turtle
+from math import *
 window = turtle.Screen
 
 #stylistic note: seac stands for Straight Edge And Compass
@@ -18,11 +19,34 @@ B = (100, 200)
 seac.setpos(B)
 seac.dot()
 
-#circle class object under construction as of October 24, 2019
-#class euCircle:
-#    def __init__():
-        
 
+class euCircle:
+    def __init__(self, center_point, radial_point, name, n=1000):
+        self.name = name
+        self.c = center_point
+        self.r_p = radial_point
+        self.radius = sqrt((self.r_p[0]-self.c[0])**2 + (self.r_p[1]-self.c[1])**2)
+
+        thetas = []
+
+        for i in range(n+1):
+            thetas.append(pi/4 + ((pi/4)/n)*i)
+
+        self.octant1 = []
+
+        for theta in thetas:
+            coordinate = (self.c[0]+(self.radius*cos(theta)), self.c[1]+(self.radius*sin(theta)))
+            self.octant1.append(coordinate)
+        self.octant2 = []
+        for j in self.octant1:
+            self.octant2.append((j[1], j[0])
+        self.quadrant1 = self.octant1 + self.octant2
+        
+       
+        
+        
+        
+testcirc = euCircle(A,B,"circAB", n=10)
 
 def circthru(ptA, ptB):
 	seac.setpos(ptB[0], ptB[1])
@@ -31,7 +55,5 @@ def circthru(ptA, ptB):
 	radius=seac.distance(ptA)
 	seac.circle(radius)
 	seac.pu()
-	
-circthru(A,B)
-circthru(B,A)
+
 turtle.exitonclick()
