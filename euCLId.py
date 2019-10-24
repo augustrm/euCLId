12377026302b7f6324cdc85c295af4fa68963b0f
augustrm <35844627+augustrm@users.noreply.github.com>
2019-10-24 11:03:54 -0600

    Update euCLId.py

--- euCLId.py
+++ euCLId.py
@@ -6,16 +6,22 @@ seac.ht()
 turtle.tracer()
 seac.home()
 seac.pu()
+
 A = (-200,100)
 seac.setpos(A)
 seac.dot()
+
 B = (100, 200)
 seac.setpos(B)
 seac.dot()
+
 def circthru(ptA, ptB):
-	seac.pd()
 	seac.setpos(ptB[0], ptB[1])
+	seac.pd()
 	seac.setheading(seac.towards(ptA)-90)
 	radius=seac.distance(ptA)
 	seac.circle(radius)
+	seac.pu()
+	
 circthru(A,B)
+circthru(B,A)
