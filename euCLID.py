8ff3744ef4c641423e20c579de8432cec9ed07ba
augustrm <35844627+augustrm@users.noreply.github.com>
2019-10-24 10:36:51 -0600

    Create euCLID.py

---
+++ euCLID.py
@@ -0,0 +1,20 @@
+import turtle
+window = turtle.Screen
+seac = turtle.Turtle()
+seac.ht()
+turtle.tracer()
+seac.home()
+seac.pu()
+A = (-200,100)
+seac.setpos(A)
+seac.dot()
+B = (100, 200)
+seac.setpos(B)
+seac.dot()
+def circthru(ptA, ptB):
+	seac.pd()
+	seac.setpos(ptB[0], ptB[1])
+	seac.setheading(seac.towards(ptA)-90)
+	radius=seac.distance(ptA)
+	seac.circle(radius)
+circthru(A,B)
