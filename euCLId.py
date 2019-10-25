eda402fc9e4157dcf4259fd691a8ef51184f4db2
augustrm <35844627+augustrm@users.noreply.github.com>
2019-10-25 16:29:19 -0600

    Update euCLId.py

--- euCLId.py
+++ euCLId.py
@@ -8,17 +8,29 @@ seac.ht()
 seac.speed(0)
 seac.home()
 seac.pu()
-
+seac.color("red")
 #define a point, its coordinates are arbitary:
-A = (-200,100)
+A = (0,23)
 seac.setpos(A)
 seac.dot()
 
 #define a point, its coordinates are arbitary:
-B = (100, 200)
+B = (200, 200)
 seac.setpos(B)
 seac.dot()
 
+seac.color("blue")
+
+class euPoint:
+	def __init__(self, x, y):
+		self.coordinates = (x,y)
+
+
+def point(x,y,name):
+	pass
+#point(100, 300, "testpoint")
+
+print(globals())
 
 class euCircle:
 	def __init__(self, center_point, radial_point, name, n=1000):
@@ -26,22 +38,10 @@ class euCircle:
 		self.c = center_point
 		self.r_p = radial_point
 		self.radius = sqrt((self.r_p[0]-self.c[0])**2 + (self.r_p[1]-self.c[1])**2)
-
-		thetas = []
-
-		for i in range(n+1):
-			thetas.append(pi/4 + ((pi/4)/n)*i)
-
-		self.octant1 = []
-
-		for theta in thetas:
-			coordinate = (self.c[0]+(self.radius*cos(theta)), self.c[1]+(self.radius*sin(theta)))
-			self.octant1.append(coordinate)
 		self.circ_coords = []
-		for j in self.octant1:
-			self.circ_coords.extend([(j[0], j[1]), (j[1], j[0]), (j[1], -j[0]), (j[0], -j[1]), (-j[0], -j[1]), (-j[1], -j[0]), (-j[1], j[0]), (-j[0], j[1])])
-			
-testcirc = euCircle(A,B,"circAB", n=10)
+		for i in range(0, n+1):
+			self.circ_coords.append((self.c[0] + cos(2*pi/n*i)*self.radius, self.c[1] + sin(2*pi/n*i)*self.radius))
+
 
 def circthru(ptA, ptB):
 	seac.setpos(ptB[0], ptB[1])
@@ -51,5 +51,6 @@ def circthru(ptA, ptB):
 	seac.circle(radius)
 	seac.pu()
 
+circthru(A,B)
+circthru(B,A)
 turtle.exitonclick()
-
