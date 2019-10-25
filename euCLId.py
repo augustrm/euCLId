86956f57931e4de99df9f927932fdb0d0cbfb21b
augustrm <35844627+augustrm@users.noreply.github.com>
2019-10-25 14:49:53 -0600

    Update euCLId.py

--- euCLId.py
+++ euCLId.py
@@ -21,31 +21,26 @@ seac.dot()
 
 
 class euCircle:
-    def __init__(self, center_point, radial_point, name, n=1000):
-        self.name = name
-        self.c = center_point
-        self.r_p = radial_point
-        self.radius = sqrt((self.r_p[0]-self.c[0])**2 + (self.r_p[1]-self.c[1])**2)
+	def __init__(self, center_point, radial_point, name, n=1000):
+		self.name = name
+		self.c = center_point
+		self.r_p = radial_point
+		self.radius = sqrt((self.r_p[0]-self.c[0])**2 + (self.r_p[1]-self.c[1])**2)
 
-        thetas = []
+		thetas = []
 
-        for i in range(n+1):
-            thetas.append(pi/4 + ((pi/4)/n)*i)
+		for i in range(n+1):
+			thetas.append(pi/4 + ((pi/4)/n)*i)
 
-        self.octant1 = []
+		self.octant1 = []
 
-        for theta in thetas:
-            coordinate = (self.c[0]+(self.radius*cos(theta)), self.c[1]+(self.radius*sin(theta)))
-            self.octant1.append(coordinate)
-        self.circ_coords = []
-        for j in self.octant1:
-            self.circ_coords.extend([(j[0], j[1]), (j[1], j[0]), (j[1], -j[0]), (j[0], -j[1]), (-j[0], -j[1]), (-j[1], -j[0]), (-j[1], j[0]), (-j[0], j[1])])
-
-        
-       
-        
-        
-        
+		for theta in thetas:
+			coordinate = (self.c[0]+(self.radius*cos(theta)), self.c[1]+(self.radius*sin(theta)))
+			self.octant1.append(coordinate)
+		self.circ_coords = []
+		for j in self.octant1:
+			self.circ_coords.extend([(j[0], j[1]), (j[1], j[0]), (j[1], -j[0]), (j[0], -j[1]), (-j[0], -j[1]), (-j[1], -j[0]), (-j[1], j[0]), (-j[0], j[1])])
+			
 testcirc = euCircle(A,B,"circAB", n=10)
 
 def circthru(ptA, ptB):
@@ -57,3 +52,4 @@ def circthru(ptA, ptB):
 	seac.pu()
 
 turtle.exitonclick()
+
