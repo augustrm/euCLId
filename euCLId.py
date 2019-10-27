07352dd47a2c0fbe176518b05e5d21ce6a5e420c
augustrm <35844627+augustrm@users.noreply.github.com>
2019-10-27 15:36:53 -0600

    updated euCLId

--- euCLId.py
+++ euCLId.py
@@ -12,20 +12,22 @@ seac.color("red")
 
 class euPoint:
 	def __init__(self, x, y, name):
-		self.coords = (x,y)
+		self.xy = (x,y)
 		self.name = name
 	def __iter__(self):
-		return((self.x, self.y))
+		yield(self.xy)
+	def __getitem__(self, i):
+		return self.xy[i]
 
 
 #define a point, its coordinates are arbitary:
 A = euPoint(0,23, "A")
-seac.setpos(A.coords)
+seac.setpos(A.xy)
 seac.dot()
 
 #define a point, its coordinates are arbitary:
 B = euPoint(200, 200, "B")
-seac.setpos(B.coords)
+seac.setpos(B.xy)
 seac.dot()
 
 seac.color("blue")
@@ -37,19 +39,19 @@ class euCircle:
 		self.c = center_point
 		self.r_p = radial_point
 		self.radius = sqrt((self.r_p[0]-self.c[0])**2 + (self.r_p[1]-self.c[1])**2)
-		self.circ_coords = []
+		self.circ_xy = []
 		for i in range(0, n+1):
-			self.circ_coords.append((self.c[0] + cos(2*pi/n*i)*self.radius, self.c[1] + sin(2*pi/n*i)*self.radius))
+			self.circ_xy.append((self.c[0] + cos(2*pi/n*i)*self.radius, self.c[1] + sin(2*pi/n*i)*self.radius))
 
 
 def circthru(ptA, ptB):
 	seac.setpos(ptB[0], ptB[1])
 	seac.pd()
 	seac.setheading(seac.towards((ptA[0],ptA[1]))-90)
-	radius=seac.distance(ptA)
+	radius = seac.distance((ptA[0],ptA[1]))
 	seac.circle(radius)
 	seac.pu()
-
+print(A[0], A[1])
 circthru(A,B)
-#circthru(B.coords,A.coords)
+#circthru(B.xy,A.xy)
 turtle.exitonclick()
