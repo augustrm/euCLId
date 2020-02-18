f5793baf038ab505fb33662f574f60405591add0
augustrm <35844627+augustrm@users.noreply.github.com>
2020-02-18 09:28:34 -0700

    update

--- parabola.py
+++ parabola.py
@@ -14,6 +14,17 @@ H = euPoint(0,0)
 I = euPoint(50,0)
 J = euPoint(75,0)
 K = euPoint(200,0)
+L = euPoint(250,0)
+
+circAE = euCircle(midpoint(A,E), A)
+circAF = euCircle(midpoint(A,F), A)
+circAG = euCircle(midpoint(A,G), A)
+circAH = euCircle(midpoint(A,H), A)
+circAI = euCircle(midpoint(A,I), A)
+circAJ = euCircle(midpoint(A,J), A)
+circAK = euCircle(midpoint(A,K), A)
+circAL = euCircle(midpoint(A,L), A)
+
 
 
 turtle.exitonclick()
