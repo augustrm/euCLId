48e5a052b3040d5652e1fc3a57f2472cc9fc3997
augustrm <35844627+augustrm@users.noreply.github.com>
2020-02-18 19:53:35 -0700

    update

--- euclid_macros.py
+++ euclid_macros.py
@@ -31,12 +31,27 @@ def angle_bisector(A,B,C,show_process=False,produce_line=False ):
 	seac.setpos(B[0], B[1])
 	dirA = seac.towards(A.xy)
 	dirC = seac.towards(C.xy)
-	if 	abs(dirA-dirC)<=90:
+	if 	abs(dirA-dirC)<90:
 		bisector = euLine(midpoint(X1,X2), midpoint(Y1,Y2), produce=produce_line)
 	elif abs(dirA-dirC)>90:
-		bisector = euLine(midpoint(X1,Y2), midpoint(Y1,X2), produce=produce_line)
-	#elif abs(dirA-dirC)==90:
-	#	bisector = euLine(midpoint(X1,X2), midpoint(Y1,Y2), produce=produce_line)
+		d = lambda P, Q : math.sqrt((P[0] - Q[0])**2 + (P[1]-Q[1])**2)
+		point_list = [X1,Y1,X2,Y2]
+		for i in point_list:
+			
+
+
+		bisector = euLine(B, midpoint(Y1,X2), produce=produce_line)
+	
+	elif (dirA-dirC)==90:
+		seac.setheading(seac.towards(A.xy)-45)
+		seac.forward(5)
+		M = euPoint(seac.xcor(), seac.ycor())
+		bisector = euLine(B,M, produce=produce_line)
+	elif (dirA-dirC)== -90:
+		seac.setheading(seac.towards(A.xy)+45)
+		seac.forward(10)
+		M = euPoint(seac.xcor(), seac.ycor())
+		bisector = euLine(B,M, produce=produce_line)
 	return  bisector
 
 
@@ -55,7 +70,7 @@ def perpendicular(X,LINE, show_process=False, produce_line=False):
 
 if __name__ == "__main__":
 	A = euPoint(0,0)
-	B = euPoint(0,-100)
+	B = euPoint(89,190)
 	C = euPoint(-200,0)
 	#M = midpoint(A,B)
 	lineAC = euLine(A,C)
@@ -64,7 +79,8 @@ if __name__ == "__main__":
 	#seac.home()
 	#lineOM = euLine(O,M)
 	#lineMA = euLine(M,A)
-	#angle_bisector(A,B,C, show_process=True)
+	#angle_bisector(A,B,C, show_process=True, produce_line=True)
 	angle_bisector(B,A,C, show_process=True, produce_line=True)
+	#angle_bisector(B,C,A, show_process=True, produce_line=True)
 	#p = perpendicular(B, lineAC, produce_line=True)
 	turtle.exitonclick()
